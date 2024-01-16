85499639fd74e2401d79fcea8ab7f0b725996188
Pavel Barashkov <Pav.Barashkov@yandex.ru>
2024-01-16 22:08:51 +0400

    add styles for OrderItemPreview

--- src/modules/BasketTable/components/OrderItemPreview/OrderItemPreview.tsx
+++ src/modules/BasketTable/components/OrderItemPreview/OrderItemPreview.tsx
@@ -1,5 +1,13 @@
 import classes from "./orderItemPreview.module.css";
+import photo from "../../коричнеКост1.jpg";
+import { Link } from "react-router-dom";
 
 export const OrderItemPreview = () => {
-  return <div className="order-item__preview">фотка</div>;
+  return (
+    <div className={classes.orderItemPreview}>
+      <Link to={'!#'}>
+        <img className={classes.orderItemPreviewImg} src={photo} alt="" />
+      </Link>
+    </div>
+    );
 };
